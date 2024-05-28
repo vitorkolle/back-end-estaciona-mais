bf687a4e2c6f25bc7f14f2a93a39b5ecd8a63c2b
Vitor Paes Kolle <123905283+vitorkolle@users.noreply.github.com>
2024-05-28 11:59:30 -0300

    Revert "CRUD DE MARCAS PRONTO"

--- controller/controller_cliente.js
+++ controller/controller_cliente.js
@@ -7,92 +7,95 @@
 const message = require('../config.js')
 const clienteDAO = require('../model/DAO/cliente.js')
 
-const getAllClientes = async function(){
+const getAllClientes = async function () {
     const clientesJSON = {}
 
     let dadosClientes = await clienteDAO.selectALLClientes()
 
-    if(dadosClientes){
-        if(dadosClientes.length > 0){
+    if (dadosClientes) {
+        if (dadosClientes.length > 0) {
             let enderecoClientes
 
-            for (let index = 0; index < dadosClientes.length; index++) { 
+            for (let index = 0; index < dadosClientes.length; index++) {
                 const element = dadosClientes[index]
 
                 enderecoClientes = await clienteDAO.selectEnderecoClientes(element.id_endereco_cliente)
+                element.endereco = enderecoClientes
             }
 
             clientesJSON.clientes = dadosClientes
-            clientesJSON.endereco = enderecoClientes
             clientesJSON.quantidade = dadosClientes.length
             clientesJSON.status_code = 200
 
             return clientesJSON
         }
-        else{
+        else {
             return message.ERROR_NOT_FOUND //404
         }
     }
-    else{
+    else {
         return message.ERROR_INTERNAL_SERVER_DB //500
     }
 }
 
-const getByIdCliente = async function(id){
+const getByIdCliente = async function (id) {
 
     let idCliente = id
 
-    if(idCliente == '' || idCliente == undefined || isNaN(idCliente)){
+    if (idCliente == '' || idCliente == undefined || isNaN(idCliente)) {
         return message.ERROR_INVALID_ID //400
     }
-    else{
-    const clienteJSON = {}
+    else {
+        const clienteJSON = {}
 
-    let dadosCliente = await clienteDAO.selectByIdCliente(id)
+        let dadosCliente = await clienteDAO.selectByIdCliente(id)
 
-    if(dadosCliente){
-        if(dadosCliente.length > 0){
-            let enderecoCliente = await clienteDAO.selectEnderecoClientes(dadosCliente[0].id_endereco_cliente)
+        if (dadosCliente) {
+            if (dadosCliente.length > 0) {
+                let enderecoCliente = await clienteDAO.selectEnderecoClientes(dadosCliente[0].id_endereco_cliente)
 
-            clienteJSON.cliente = dadosCliente
-            clienteJSON.endereco = enderecoCliente
-            clienteJSON.status_code = 200
+                clienteJSON.cliente = dadosCliente
+                clienteJSON.endereco = enderecoCliente
+                clienteJSON.status_code = 200
 
-            return clienteJSON
+                return clienteJSON
 
-        }else{
-            return message.ERROR_NOT_FOUND //404
+            } else {
+                return message.ERROR_NOT_FOUND //404
+            }
+        } else {
+            return message.ERROR_INTERNAL_SERVER_DB //500
         }
-    }else{
-        return message.ERROR_INTERNAL_SERVER_DB //500
     }
 }
-}
 
-const setInserirCliente = async function(dadosCliente, contentType){
+const setInserirCliente = async function (dadosCliente, contentType) {
     try {
-        if (String(contentType).toLowerCase() == 'application/json'){
+        if (String(contentType).toLowerCase() == 'application/json') {
             let novoClienteJSON = {}
 
-            if(
-                dadosCliente.nome  ==  ''            || dadosCliente.nome == null            || dadosCliente.nome == undefined            || dadosCliente.nome.length > 80            ||
-                dadosCliente.data_nascimento  ==  '' || dadosCliente.data_nascimento == null || dadosCliente.data_nascimento == undefined || dadosCliente.data_nascimento.length > 10 ||
-                dadosCliente.cpf  ==  ''             || dadosCliente.cpf == null             || dadosCliente.cpf == undefined             || dadosCliente.cpf.length > 14             ||
-                dadosCliente.email  ==  ''           || dadosCliente.email == null           || dadosCliente.email == undefined           || dadosCliente.email.length > 100          ||
-                dadosCliente.senha  ==  ''           || dadosCliente.senha == null           || dadosCliente.senha == undefined           || dadosCliente.senha.length > 20           ||
-                dadosCliente.telefone  ==  ''        || dadosCliente.telefone == null        || dadosCliente.telefone == undefined        || dadosCliente.telefone.length > 15        ||
-                dadosCliente.logradouro  ==  ''      || dadosCliente.logradouro == null      || dadosCliente.logradouro == undefined      || dadosCliente.logradouro.length > 100     ||
-                dadosCliente.bairro  ==  ''          || dadosCliente.bairro == null          || dadosCliente.bairro == undefined          || dadosCliente.bairro.length > 50          ||
-                dadosCliente.cidade  ==  ''          || dadosCliente.cidade == null          || dadosCliente.cidade == undefined          || dadosCliente.cidade.length > 100         ||
-                dadosCliente.estado  ==  ''          || dadosCliente.estado == null          || dadosCliente.estado == undefined          || dadosCliente.estado.length > 40          ||
-                dadosCliente.pais  ==  ''            || dadosCliente.pais == null            || dadosCliente.pais == undefined            || dadosCliente.pais.length > 40            
+            if (
+                dadosCliente.nome == '' || dadosCliente.nome == null || dadosCliente.nome == undefined || dadosCliente.nome.length > 80 ||
+                dadosCliente.data_nascimento == '' || dadosCliente.data_nascimento == null || dadosCliente.data_nascimento == undefined || dadosCliente.data_nascimento.length > 10 ||
+                dadosCliente.cpf == '' || dadosCliente.cpf == null || dadosCliente.cpf == undefined || dadosCliente.cpf.length > 14 ||
+                dadosCliente.email == '' || dadosCliente.email == null || dadosCliente.email == undefined || dadosCliente.email.length > 100 ||
+                dadosCliente.senha == '' || dadosCliente.senha == null || dadosCliente.senha == undefined || dadosCliente.senha.length > 20 ||
+                dadosCliente.telefone == '' || dadosCliente.telefone == null || dadosCliente.telefone == undefined || dadosCliente.telefone.length > 15 ||
+                dadosCliente.logradouro == '' || dadosCliente.logradouro == null || dadosCliente.logradouro == undefined || dadosCliente.logradouro.length > 100 ||
+                dadosCliente.bairro == '' || dadosCliente.bairro == null || dadosCliente.bairro == undefined || dadosCliente.bairro.length > 50 ||
+                dadosCliente.cidade == '' || dadosCliente.cidade == null || dadosCliente.cidade == undefined || dadosCliente.cidade.length > 100 ||
+                dadosCliente.estado == '' || dadosCliente.estado == null || dadosCliente.estado == undefined || dadosCliente.estado.length > 40 ||
+                dadosCliente.pais == '' || dadosCliente.pais == null || dadosCliente.pais == undefined || dadosCliente.pais.length > 40
 
-            ){
+            ) {
                 return message.ERROR_REQUIRED_FIELDS //400
             }
-            else{
+            else {
                 let novoCliente = await clienteDAO.insertCliente(dadosCliente)
-                
+                let idNovoCliente = await clienteDAO.selectLastIdClientes()
+
+                console.log(idNovoCliente);
+
                 let enderecoJSON = {}
                 enderecoJSON.logradouro = dadosCliente.logradouro
                 enderecoJSON.bairro = dadosCliente.bairro
@@ -100,18 +103,19 @@ const setInserirCliente = async function(dadosCliente, contentType){
                 enderecoJSON.estado = dadosCliente.estado
                 enderecoJSON.pais = dadosCliente.pais
 
-                if(novoCliente){
+                if (novoCliente) {
+                    novoClienteJSON.id = Number(idNovoCliente[0].id)
                     novoClienteJSON.cliente = dadosCliente
                     novoClienteJSON.endereco = enderecoJSON
                     novoClienteJSON.status_code = message.SUCCESS_CREATED_ITEM.status_code //200
                     novoClienteJSON.message = message.SUCCESS_CREATED_ITEM.message
 
                     return novoClienteJSON
-                }else{
+                } else {
                     return message.ERROR_INTERNAL_SERVER_DB //500
                 }
             }
-        }else{
+        } else {
             return message.ERROR_CONTENT_TYPE //415
         }
     } catch (error) {
@@ -119,15 +123,27 @@ const setInserirCliente = async function(dadosCliente, contentType){
     }
 }
 
-const setDeletarClientes = async function(id){
+const setDeletarClientes = async function (id) {
     let idClientes = id
 
-    let validarID = getByIdCliente(idClientes)
+    let validarID = await clienteDAO.selectByIdCliente(idClientes)
+    if (validarID.length > 0) {
 
-    if(validarID){
+        if (idClientes == '' || idClientes == null || isNaN(idClientes)) {
+            return message.ERROR_INVALID_ID //400
+        }
+        else {
+            let deleteCliente = await clienteDAO.deleteCliente(idClientes)
 
-    }else{
-        
+            if (deleteCliente) {
+                return message.SUCCESS_DELETED_ITEM //200
+            }
+            else{
+                return message.ERROR_INTERNAL_SERVER_DB //500
+            }
+        }
+    } else {
+        return message.ERROR_NOT_FOUND //404
     }
 }
 
@@ -135,5 +151,6 @@ const setDeletarClientes = async function(id){
 module.exports = {
     getAllClientes,
     getByIdCliente,
-    setInserirCliente
+    setInserirCliente,
+    setDeletarClientes
 }
--- controller/controller_tipoVeiculo.js
+++ controller/controller_tipoVeiculo.js
@@ -24,21 +24,19 @@ const getListarTiposVeiculos = async function () {
             return veiculoJSON
         }
         else {
-            return message.ERROR_INTERNAL_SERVER_DB
+            return message.ERROR_NOT_FOUND //404
         }
     }
-
     else {
-        return message.ERROR_INTERNAL_SERVER
+        return message.ERROR_INTERNAL_SERVER_DB //500
     }
 
 }
 
-const getBuscarNomeVeiculo = async function (tipo_veiculo) {
-
+const getBuscarVeiculo = async function(id) {
     const veiculoJSON = {}
 
-    let dadosVeiculo = await tipoVeiculoDAO.selectByNomeVeiculo(tipo_veiculo)
+    let dadosVeiculo = await tipoVeiculoDAO.selectByIdVeiculo(id)
 
     if (dadosVeiculo) {
         if (dadosVeiculo.length > 0) {
@@ -49,103 +47,72 @@ const getBuscarNomeVeiculo = async function (tipo_veiculo) {
             return veiculoJSON
         }
         else {
-            return message.ERROR_INTERNAL_SERVER_DB //500
+            return message.ERROR_NOT_FOUND//404
         }
-
     }
     else {
-        return message.ERROR_INTERNAL_SERVER  //500     
+        return message.ERROR_INTERNAL_SERVER_DB//500     
     }
-
-
-
 }
 
 const setInserirTipoVeiculo = async function (dadosVeiculo, contentType) {
-
     try {
-
-
-
         if (String(contentType).toLowerCase() == 'application/json') {
-
             //objeto JSON de Ator
             const veiculoJSON = {}
 
-
             if (dadosVeiculo.tipo_veiculo == '' || dadosVeiculo.tipo_veiculo == null || dadosVeiculo.tipo_veiculo == undefined || dadosVeiculo.tipo_veiculo.length > 20) {
+                return message.ERROR_REQUIRED_FIELDS //400
+            }
+            else {
+                let tipoVeiculoNovo = await tipoVeiculoDAO.insertTipoVeiculo(dadosVeiculo)
+                let idVeiculo = await tipoVeiculoDAO.selectLastIdTipoVeiculo()
 
-                    return message.ERROR_REQUIRED_FIELDS //400
+                if (tipoVeiculoNovo) {
+                    veiculoJSON.id = Number(idVeiculo[0].id)
+                    veiculoJSON.tipo_veiculo = dadosVeiculo.tipo_veiculo
+                    veiculoJSON.status_code = message.SUCCESS_CREATED_ITEM.status_code
+                    veiculoJSON.message = message.SUCCESS_CREATED_ITEM.message
 
-            } else{
-                let validadeStatus = true
-
-                if(validadeStatus){
-                    let tipoVeiculoNovo = await tipoVeiculoDAO.insertTipoVeiculo(dadosVeiculo)
-
-                  console.log(tipoVeiculoNovo);
-                    if(tipoVeiculoNovo){
-                        veiculoJSON.file = dadosVeiculo
-                        veiculoJSON.quantidade = dadosVeiculo.length
-                        veiculoJSON.status = message.SUCCESS_CREATED_ITEM.status
-                        veiculoJSON.status_code = message.SUCCESS_CREATED_ITEM.status_code
-                        veiculoJSON.message = message.SUCCESS_CREATED_ITEM
-
-                        return veiculoJSON
-                        
-                    } else {
-                       
-                        return message.ERROR_INTERNAL_SERVER_DB //500
-                    }
+                    return veiculoJSON
+                }
+                else {
+                    return message.ERROR_INTERNAL_SERVER_DB //500
                 }
             }
         } else {
-           
             return message.ERROR_CONTENT_TYPE //415
         }
     } catch {
-         return message.ERROR_INTERNAL_SERVER
+        return message.ERROR_INTERNAL_SERVER //500 controller
     }
 }
 
-const setAtualizarVeiculo = async function(dadosVeiculo, contentType, id){
-    
+const setAtualizarVeiculo = async function (dadosVeiculo, contentType, id) {
     //Validação do content-Type da requisição  
     try {
-
         if (String(contentType).toLowerCase() == 'application/json') {
-
-
             //Cria o objeto JSON para devolver 
             let novoTipoVeiculoJSON = {}
-
-
-
             //Validação de campos obrigatórios ou com digitação inválida 
-            if (dadosVeiculo.tipo_veiculo == '' || dadosVeiculo.tipo_veiculo == undefined || dadosVeiculo.tipo_veiculo == null || dadosVeiculo.tipo_veiculo.length > 20 
-                
-            ) {
-
+            if (dadosVeiculo.tipo_veiculo == '' || dadosVeiculo.tipo_veiculo == undefined || dadosVeiculo.tipo_veiculo == null || dadosVeiculo.tipo_veiculo.length > 20) {
                 return message.ERROR_REQUIRED_FIELDS; //400
-
             }
             else {
+                let validarId = await tipoVeiculoDAO.selectByIdVeiculo(id)
 
+                if(validarId.length > 0){
+                    //Validação para verificar se podemos encaminhar os dados para o DAO
+                    dadosVeiculo.id = id
 
-
-                let validateStatus = true;
-
-
-                //Validação para verificar se podemos encaminhar os dados para o DAO
-                if (validateStatus) {
-                    const idV = dadosVeiculo.id = id;
                     //encaminha os dados da classificação para o DAo inserir no BD
                     let tipoAtualizado = await tipoVeiculoDAO.updateTipoVeiculo(dadosVeiculo)
 
                     //Validação para verificar se o DAO inseriu os dados do BD
-                    if (tipoAtualizado & idV) {
+                    if (tipoAtualizado) {
                         //Cria o JSON de retorno dos dados(201)
-                        novoTipoVeiculoJSON.file = dadosVeiculo;
+                        novoTipoVeiculoJSON.id = dadosVeiculo.id
+                        novoTipoVeiculoJSON.tipo_veiculo = dadosVeiculo.tipo_veiculo
                         novoTipoVeiculoJSON.status = message.SUCCESS_CREATED_ITEM.status;
                         novoTipoVeiculoJSON.status_code = message.SUCCESS_CREATED_ITEM.status_code;
                         novoTipoVeiculoJSON.message = message.SUCCESS_CREATED_ITEM.message;
@@ -154,12 +121,10 @@ const setAtualizarVeiculo = async function(dadosVeiculo, contentType, id){
                     } else {
                         return message.ERROR_INTERNAL_SERVER_DB //500
                     }
-
                 }
-                else {
-                    return message.ERROR_NOT_FOUND //404
+                else{
+                    return message.ERROR_INVALID_ID //400
                 }
-
             }
         }
 
@@ -172,39 +137,37 @@ const setAtualizarVeiculo = async function(dadosVeiculo, contentType, id){
 
 }
 
-const setExcluirTipoVeiculo = async function(id){
+const setExcluirTipoVeiculo = async function (id) {
     try {
         //id das classificações
         let idV = id
 
-
-
-        if (idV == null || idV == undefined || idV == '') {
-
         if (idV == null || idV == undefined || idV == '' || isNaN(idV)) {
-
             return message.ERROR_REQUIRED_FIELDS //400
-        } else {
-            let dadosVeiculo = await tipoVeiculoDAO.deleteTipoVeiculo(idV)
+        }
+        else {
+            let validarId = await tipoVeiculoDAO.selectByIdVeiculo(id)
 
+            if(validarId){
+                let dadosVeiculo = await tipoVeiculoDAO.deleteTipoVeiculo(idV)
 
-            if (dadosVeiculo) {
-                return message.SUCCESS_DELETED_ITEM //201
-            } else {
-                return message.ERROR_INTERNAL_SERVER_DB //500
+                if (dadosVeiculo) {
+                    return message.SUCCESS_DELETED_ITEM //200
+                } else {
+                    return message.ERROR_INTERNAL_SERVER_DB //500
+                }
+            }else{
+                return message.ERROR_NOT_FOUND //404
             }
         }
-
     } catch (error) {
-
-        return message.ERROR_INTERNAL_SERVER_DB //500
-
+        return message.ERROR_INTERNAL_SERVER //500 controller
     }
-    }
+}
 
 module.exports = {
     getListarTiposVeiculos,
-    getBuscarNomeVeiculo,
+    getBuscarVeiculo,
     setInserirTipoVeiculo,
     setAtualizarVeiculo,
     setExcluirTipoVeiculo
